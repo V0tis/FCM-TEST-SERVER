77229909092c5bf973c69aa7426e1f9a9519e25e
JungDonggun <donggun.dev@gmail.com>
2020-04-03 12:31:07 +0900

    REFACTOR PushFCM

--- src/api/Push/PushFCM.resolvers.ts
+++ src/api/Push/PushFCM.resolvers.ts
@@ -1,17 +1,23 @@
 import { Resolvers } from "../../resolvers";
 import { PushFCMResponse } from "../../types/graphql";
-import admin from 'firebase-admin'
 import sendMessage from "../../utils/sendMessage";
+import admin from "firebase-admin";
+import MessagingDevicesResponse = admin.messaging.MessagingDevicesResponse;
 
 const resolvers: Resolvers = {
   Query: {
     pushFCM: async (parent, args, info): Promise<PushFCMResponse> => {
       try {
-        console.log(sendMessage({
+        const requestMessage: MessagingDevicesResponse | undefined = await sendMessage({
           title: 'hi',
           body: 'this is test',
           token: 'jkajdjafjqnemrgnrqmgn14t1j4t1-14t-0n4mjn'
-        }))
+        })
+
+        if (requestMessage !== undefined) {
+          console.log('requestMessage', requestMessage.results)
+        }
+
         return {
           status: true
         }
